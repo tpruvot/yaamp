cab02a0417f180000b2250bc501692525d6ed396
Tristian <lavorata@gmail.com>
2017-05-11 16:28:14 -0400

    nova: get deposit addresses

--- web/yaamp/core/backend/markets.php
+++ web/yaamp/core/backend/markets.php
@@ -935,6 +935,29 @@ function updateNovaMarkets()
 				break;
 			}
 		}
+
+		if(!empty(EXCH_NOVA_KEY))
+		{
+			$last_checked = cache()->get($exchange.'-deposit_address-check-'.$coin->symbol);
+			if(empty($market->deposit_address) && !$last_checked)
+			{
+				sleep(1);
+				$res = nova_api_user('getdepositaddress/'.$coin->symbol);
+				if($res->status == 'success') {
+					$addr = arraySafeVal($res, 'address');
+					if (!empty($res)) {
+						$market->deposit_address = $addr;
+						// delimiter "::" for memo / payment id
+						$market->message = null;
+						debuglog("$exchange: deposit address for {$coin->symbol} updated");
+						$market->save();
+					} else {
+						debuglog("$exchange: Failed to update deposit address, ".json_decode($res));
+					}
+				}
+				cache()->set($exchange.'-deposit_address-check-'.$coin->symbol, time(), 24*3600);
+			}
+		}
 	}
 }
 
