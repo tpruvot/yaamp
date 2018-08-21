01dca49636d37fc8d828360fb9a9755fb0ce88f2
phm87 <31578435+phm87@users.noreply.github.com>
2018-08-21 03:11:06 +0200

    Update api.php

--- web/yaamp/modules/site/api.php
+++ web/yaamp/modules/site/api.php
@@ -45,9 +45,24 @@ result:
 		"accepted": 82463372.083,
 		"rejected": 0
 	}]
+<?php
+if (YAAMP_API_PAYOUTS)
+{
+	?>
+	"payouts":[{
+		"time": "1529860641",
+		"amount": "0.001",
+		"tx": "transaction_id_of_the_payout"
+	}]
+	<?php
+}
+?>
 }
 </pre>
-
+<?php
+if (YAAMP_API_PAYOUTS)
+	echo "Please note that only the payouts of the last ".(YAAMP_API_PAYOUTS_PERIOD / 3600)." hours are displayed.";
+?>
 <p><b>Pool Status</b></p>
 
 request:
