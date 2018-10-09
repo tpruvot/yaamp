ca67310a192719feae23d8f6020397dab27eab84
zhq <32982289+zhq1@users.noreply.github.com>
2018-10-09 21:18:07 +0800

    Update stratum.h

--- stratum/stratum.h
+++ stratum/stratum.h
@@ -210,4 +210,3 @@ void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 #include "algos/hex.h"
 #include "algos/argon2d-dyn.h"
 #include "algos/renesis.h"
-#include "algos/lyra2z330.h
