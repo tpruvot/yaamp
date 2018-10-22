4085445b7d26861304f7294c62a3e99b8eebdad3
Sakda Chutchawan <oddhome@gmail.com>
2018-10-22 15:19:53 +0700

    add GLT support coinbase

--- stratum/coinbase.cpp
+++ stratum/coinbase.cpp
@@ -262,6 +262,48 @@ void coinbase_create(YAAMP_COIND *coind, YAAMP_JOB_TEMPLATE *templ, json_value *
             return;
         }
 
+    else if(strcmp(coind->symbol, "GLT") == 0)
+	{
+		char script_dests[2048] = { 0 };
+		char script_payee[128] = { 0 };
+        char script_treasury[128] = { 0 };
+		char payees[4];
+		int npayees = 1;
+		bool masternode_enabled = json_get_bool(json_result, "masternode_payments_enforced");
+		json_value* masternode = json_get_object(json_result, "masternode");
+        json_value* treasury = json_get_object(json_result, "treasury");
+        bool treasury_enabled = true;
+		if(treasury_enabled && treasury) {
+				const char *scriptPubKey = json_get_string(treasury, "scriptPubKey");
+                memcpy(script_treasury, &scriptPubKey, sizeof script_treasury);
+				json_int_t amount = json_get_int(treasury, "amount");
+				if (scriptPubKey && amount) {
+					npayees++;
+					available -= amount;
+					job_pack_tx(coind, script_dests, amount, script_treasury);
+					//debuglog("%s treasury %u\n", coind->symbol, amount);
+				}
+		}
+		if (masternode_enabled && masternode) {
+			const char *payee = json_get_string(masternode, "payee");
+			json_int_t amount = json_get_int(masternode, "amount");
+			if (payee && amount) {
+				npayees++;
+				available -= amount;
+				base58_decode(payee, script_payee);
+				job_pack_tx(coind, script_dests, amount, script_payee);
+			}
+		}
+		sprintf(payees, "%02x", npayees);
+		strcat(templ->coinb2, payees);
+		strcat(templ->coinb2, script_dests);
+		job_pack_tx(coind, templ->coinb2, available, NULL);
+		strcat(templ->coinb2, "00000000"); // locktime
+		coind->reward = (double)available/100000000*coind->reward_mul;
+		//debuglog("%s %d dests %s\n", coind->symbol, npayees, script_dests);
+		return;
+	}
+
 	else if(strcmp(coind->symbol, "STAK") == 0) {
 		char script_payee[512] = { 0 };
 		char payees[4];
