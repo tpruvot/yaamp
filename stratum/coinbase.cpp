ba94cbe38700e88adecbb2088180568e18fc4b3a
Sakda Chutchawan <oddhome@gmail.com>
2018-11-15 17:05:48 +0700

    add support AGM

--- stratum/coinbase.cpp
+++ stratum/coinbase.cpp
@@ -304,7 +304,7 @@ void coinbase_create(YAAMP_COIND *coind, YAAMP_JOB_TEMPLATE *templ, json_value *
 	}
 
 	//  add IFX
-	if ((strcmp(coind->symbol, "IFX") == 0)||(strcmp(coind->symbol, "GTM") == 0))
+	if ((strcmp(coind->symbol, "IFX") == 0)||(strcmp(coind->symbol, "GTM") == 0)||(strcmp(coind->symbol, "AGM") == 0))
 	{
 		char payees[4];
 		int npayees = 1;
