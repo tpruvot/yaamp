b080f799651cba559b10498cc677dbffe05e199c
cryptopool <cryptopool.builders@gmail.com>
2020-04-09 17:49:10 -0400

    dif is gonna be the death of me

--- stratum/client_difficulty.cpp
+++ stratum/client_difficulty.cpp
@@ -52,8 +52,8 @@ void client_adjust_difficulty(YAAMP_CLIENT *client)
 		return;
 	}
 
-	if(client->shares_per_minute > 80)
-		client_change_difficulty(client, client->difficulty_actual*1.5);
+	if(client->shares_per_minute > 100)
+		client_change_difficulty(client, client->difficulty_actual*4);
 
 	else if(client->difficulty_fixed)
 		return;
@@ -61,8 +61,9 @@ void client_adjust_difficulty(YAAMP_CLIENT *client)
 	else if(client->shares_per_minute > 25)
 		client_change_difficulty(client, client->difficulty_actual*2);
 
-	else if(client->shares_per_minute > 10)
+	else if(client->shares_per_minute > 20)
 		client_change_difficulty(client, client->difficulty_actual*1.5);
+
 	else if(client->shares_per_minute <  5)
 		client_change_difficulty(client, client->difficulty_actual/2);
 }
