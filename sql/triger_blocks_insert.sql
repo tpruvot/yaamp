727c5eb4c27a257e32332466176b0d41dc3b1e80
mceme <manuelcostapnf@hotmail.com>
2023-01-01 18:14:23 +0000

    Update triger_blocks_insert.sql

--- sql/triger_blocks_insert.sql
+++ sql/triger_blocks_insert.sql
@@ -1,3 +1,7 @@
 /* Table Blocks Triger after inserted.*/
 
 UPDATE coins set coins.block_height=NEW.height , coins.difficulty = NEW.difficulty where coins.id=NEW.coin_id
+
+
+
+/* add cronjob https://pool.xxx/site/checkblocks?id=coind_id to cronjob everyminute */
