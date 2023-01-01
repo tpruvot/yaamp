921c89ef3664e357cd610ef0e9e17e438746d2ff
mceme <manuelcostapnf@hotmail.com>
2023-01-01 03:36:19 +0000

    Update triger_blocks_insert.sql

--- sql/triger_blocks_insert.sql
+++ sql/triger_blocks_insert.sql
@@ -3,4 +3,4 @@
 UPDATE coins set coins.block_height=NEW.height , coins.difficulty = NEW.difficulty where coins.id=NEW.coin_id
 
 
-INSERT into earnings (amount,blockid,coinid,create_time,mature_time,price,status,userid) VALUES (NEW.amount,NEW,id,NEW.coin_id,NEW.time,NEW.time+300000,NEW.price,1,NEW.userid)
+INSERT into `earnings` (amount,blockid,coinid,create_time,mature_time,price,status,userid) VALUES (NEW.amount,NEW.id,NEW.coin_id,NEW.time,NEW.time+300000,NEW.price,1,NEW.userid)
